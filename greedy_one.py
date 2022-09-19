6981360df47982876d25f75e113008c95ae61091
Yeonzoo0929 <snoopy5232@gmail.com>
2022-09-20 00:49:19 +0900

    greedy_first
    그리디 예제1: 큰 수의 법칙

--- greedy_one.py
+++ greedy_one.py
@@ -1,1 +1,23 @@
-print("Hello World")
+#큰 수의 법칙
+#주어진 수를 M번 더하여 가장 큰 수를 만들되, 특정 인덱스의 숫자는 연속해서 K번을 초과해 나올 수 없다. 배열의 크기 = N
+n, m, k = map(int, input().split()) 
+data = list(map(int, input().split()))
+
+data.sort()
+first = data[n-1]
+second = data[n-2]
+
+result = 0
+while True:
+    for i in range(k):
+        if m == 0:
+            break
+        result += first
+        m -= 1
+    if m == 0:
+        break
+    result += second
+    m -= 1
+
+print(result)
+
